86987974018bd528fb07be11c8d62990be839cce
Jamie-Shelter <70642894+Jamie-Shelter@users.noreply.github.com>
2021-08-02 14:54:10 +0100

    js stuff
    it's js to add all the inputs together

---
+++ main.js
@@ -0,0 +1,86 @@
+// function getInputValue(input) {
+//   var inputVal = document.getElementById(input).value;
+// }
+//
+// function hasParameters(url) {
+//   url.includes('?')
+// }
+
+
+function addUrlDecorator() {
+  var connector,
+      url = document.getElementById('url-input').value;
+  if ( url.includes('?') ) {
+    connector = '&'
+  } else {
+    connector = '?'
+  };
+  return url + connector
+}
+
+function addAcSource() {
+  var utmAc = document.getElementById('ac-input').value;
+  if (utmAc != '') {
+    return 'reserved_appeal_code=' + utmAc.split(' ').join('&')
+  } else {
+    return ''
+  }
+}
+
+function addUtmSource() {
+  var utmSource = document.getElementById('source-input').value;
+  if (utmSource != '') {
+    return 'utm_source=' + utmSource.split(' ').join('%20')
+  } else {
+    return ''
+  }
+}
+
+function addUtmMedium() {
+  var utmMedium = document.getElementById('medium-input').value;
+  if (utmMedium != '') {
+    return '&utm_medium=' + utmMedium.split(' ').join('%20')
+  } else {
+    return ''
+  }
+}
+
+function addUtmCampaign() {
+  var utmCampaign = document.getElementById('campaign-input').value;
+  if (utmCampaign != '') {
+    return '&utm_campaign=' + utmCampaign.split(' ').join('%20')
+  } else {
+    return ''
+  }
+}
+
+function addUtmContent() {
+  var utmContent = document.getElementById('content-input').value;
+  if (utmContent != '') {
+    return '&utm_content=' + utmContent.split(' ').join('%20')
+  } else {
+    return ''
+  }
+}
+
+function addUtmTerm() {
+  var utmTerm = document.getElementById('term-input').value;
+  if (utmTerm != '') {
+    return '&utm_term=' + utmTerm.split(' ').join('%20')
+  } else {
+    return ''
+  }
+}
+
+function createUtm() {
+  var utm = addUrlDecorator() + addAcSource() + addUtmSource() + addUtmMedium() + addUtmCampaign() + addUtmContent() + addUtmTerm();
+  document.getElementById("utm").value = utm;
+}
+
+function copy() {
+  var utmValue = document.getElementById('utm'),
+      copyButtonValue = document.getElementById('copy');
+  utmValue.select();
+  document.execCommand('copy');
+  copyButtonValue.innerHTML = "Copy UTM";
+}
